8d6d2badd15b165a3bd84fadb6dbc50c9f398526
Gwendolyn Zubatch <gyzubatch@gmail.com>
2018-10-25 14:06:14 -0700

    arrays defined; function created; guessgame.js only

--- guessgame.js
+++ guessgame.js
@@ -1,49 +1,75 @@
-'use strict ""';
+'use strict';
 
-//Alert user to answer is only yes or no
-alert('Please respond to the following questions with y, yes, Y, YES.');
+function andrewQuestions (questionNumber) {
+  //array for questions
+  var questions = [0, 'Is my name Andrew?', 'Am I 33 years old?', 'Was I born is Seattle?', 'Is my favorite food tacos?', 'Am I a student at Code Fellows?'];
+  //array for answers
+  var correctAnswers = [0, 'YES', 'YES', 'YES', 'YES', 'YES'];
+  //array for positive feedback to user
+  var positiveFeedback = [0, 'You\'re correct!', 'That\'s correct. Where\'s my birth present?', 'Yes, that\'s my city!', 'Of course I do; I\'m Mexican.', 'I am! That\'s how I\'m learning all this cool stuff.'];
+  //array for negative feedback
+  var constructiveFeedback = [0, 'Shaking my head...', 'Nope. No wonder you weren\'t at my birthday party.', 'Although I grew up in Eastern WA, I was born Seattle.', 'Wrong choice. Now I\'m not sharing my tacos with you.', 'Incorrect, I\'m obviously a beginner.'];
 
-//Q1
-var userName = prompt('Is my name Andrew?').toUpperCase();
-console.log('userName', userName);
-if (userName === 'Y' || userName === 'YES') {
-  alert('You\'re correct!');
-} else {
-  alert('Shaking my head...');}
+  //prompt with question and log response
+  var response = prompt('Is my name Andrew?').toUpperCase();
+  console.log('userName', userName);
+  //check response against correct answer
+  if (userName === 'Y' || userName === 'YES') {
+    //tell user whether correct
+    alert('You\'re correct!');
+    var correctTally = 1;
+  } else {
+    //tell user whether correct
+    alert('Shaking my head...');
+    var correctTally = 0;
+  }
+  //return correct boolean (for use in total correct tally)
+}
 
-//Q2
-var userAge = prompt('Am I 33 years old?').toUpperCase();
-console.log('userAge', userAge);
-if (userAge === 'Y' || userAge === 'YES') {
-  alert('That\'s correct. Where\'s my birth present?');
-} else {
-  alert('Nope. No wonder you weren\'t at my birthday party.');}
+// //Alert user to answer is only yes or no
+// alert('Please respond to the following questions with y, yes, Y, YES.');
 
-//Q3
-var userBirthPlace = prompt('Was I born is Seattle?').toUpperCase();
-console.log('userBirthPlace', userBirthPlace);
-if (userBirthPlace === 'Y' || userBirthPlace === 'YES') {
-  alert('Yes, that\'s my city!');
-} else {
-  alert('Although I grew up in Eastern WA, I was born Seattle.');}
+// //Q1
+// var userName = prompt('Is my name Andrew?').toUpperCase();
+// console.log('userName', userName);
+// if (userName === 'Y' || userName === 'YES') {
+//   alert('You\'re correct!');
+// } else {
+//   alert('Shaking my head...');}
 
-//Q4
-var userFood = prompt('Is my favorite food tacos?').toUpperCase();
-console.log('userFood', userFood);
-if (userFood === 'Y' || userFood=== 'YES') {
-  alert('Of course I do; I\'m Mexican.');
-} else {
-  alert('Wrong choice. Now I\'m not sharing my tacos with you.');}
+// //Q2
+// var userAge = prompt('Am I 33 years old?').toUpperCase();
+// console.log('userAge', userAge);
+// if (userAge === 'Y' || userAge === 'YES') {
+//   alert('That\'s correct. Where\'s my birth present?');
+// } else {
+//   alert('Nope. No wonder you weren\'t at my birthday party.');}
 
-//Q5
-var userSchool = prompt('Am I a student at Code Fellows?').toUpperCase();
-console.log('userSchool', userSchool);
-if (userSchool === 'Y' || userSchool === 'YES') {
-  alert('I am! That\'s how I\'m learning all this cool stuff.');
-} else {
-  alert('Incorrect, I\'m obviously a beginner.');}
+// //Q3
+// var userBirthPlace = prompt('Was I born is Seattle?').toUpperCase();
+// console.log('userBirthPlace', userBirthPlace);
+// if (userBirthPlace === 'Y' || userBirthPlace === 'YES') {
+//   alert('Yes, that\'s my city!');
+// } else {
+//   alert('Although I grew up in Eastern WA, I was born Seattle.');}
 
-alert('Now, you will guess the final two questions.');
+// //Q4
+// var userFood = prompt('Is my favorite food tacos?').toUpperCase();
+// console.log('userFood', userFood);
+// if (userFood === 'Y' || userFood=== 'YES') {
+//   alert('Of course I do; I\'m Mexican.');
+// } else {
+//   alert('Wrong choice. Now I\'m not sharing my tacos with you.');}
+
+// //Q5
+// var userSchool = prompt('Am I a student at Code Fellows?').toUpperCase();
+// console.log('userSchool', userSchool);
+// if (userSchool === 'Y' || userSchool === 'YES') {
+//   alert('I am! That\'s how I\'m learning all this cool stuff.');
+// } else {
+//   alert('Incorrect, I\'m obviously a beginner.');}
+
+// alert('Now, you will guess the final two questions.');
 
 // Q6
 var sportsNumber = 34;
